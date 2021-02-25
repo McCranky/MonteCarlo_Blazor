f3e9a0b4e55dc569f20fa2f8d32e4156367a8f5d
McCranky <cr4nky97@gmail.com>
2021-02-25 18:17:37 +0100

    Robit prototype

---
+++ Models/RobitResult.cs
@@ -0,0 +1,17 @@
+﻿namespace MonteCarlo_Blazor.Models
+{
+    public class RobitResult
+    {
+        public double AverageSteps { get; set; }
+        public double AverageProbability { get; set; }
+
+        public static RobitResult operator /(RobitResult rr, double c)
+        {
+            return new RobitResult 
+            { 
+                AverageProbability = rr.AverageProbability / c, 
+                AverageSteps = rr.AverageSteps / c 
+            };
+        }
+    }
+}
---
+++ Models/RobitSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MonteCarlo_Blazor.Models
+{
+    public class RobitSettings
+    {
+        [Required]
+        public MonteCarloSettings MonteCarlo { get; set; } = new MonteCarloSettings();
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int XNodes { get; set; } = 4;
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int YNodes { get; set; } = 4;
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int KSteps { get; set; } = 4;
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int StartX { get; set; }
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int StartY { get; set; }
+    }
+}
